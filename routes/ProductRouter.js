790904fc9f461c5e466090b64ca7236a00afcb2e
Dip Jit Baroi <dipjitbaroiofficial@gmail.com>
2024-12-04 16:07:20 +0600

    feature: 🍎 added new controller and required route

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const { CreateProduct, GetAllProduct, GetProductByID, SearchProduct } = require("../controller/ProductController");
+const {
+  CreateProduct,
+  GetAllProduct,
+  GetProductByID,
+  GetProducts,
+} = require("../controller/ProductController");
 const router = express.Router();
 
+router.post("/Product/Create", CreateProduct);
+router.get("/Product/All", GetAllProduct);
+router.get("/Product/:id", GetProductByID);
 
-router.post("/Product/Create", CreateProduct );
-router.get("/Product/All", GetAllProduct );
-router.get("/Product/:id", GetProductByID );
-
-router.get("/Product/:category/:brand/:keyword", SearchProduct);
-
+router.get("/Products", GetProducts);
 
 module.exports = router;
